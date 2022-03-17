00b4698d0fed3527ee5acd452d38dc24da25ac48
sosasamu <sosamaximilianosamuel@gmail.com>
2022-03-17 17:27:16 -0300

    Remove duplicates

--- android/src/main/java/com/salesforce/marketingcloud/reactnative/RNMarketingCloudSdk.java
+++ android/src/main/java/com/salesforce/marketingcloud/reactnative/RNMarketingCloudSdk.java
@@ -32,11 +32,6 @@ import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.salesforce.marketingcloud.MCLogListener;
 import com.salesforce.marketingcloud.MarketingCloudSdk;
-import java.util.Collections;
-import java.util.List;
-
-import android.app.Activity;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
